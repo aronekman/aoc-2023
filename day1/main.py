f440e071f71fcc3fb1f200d6acdc4f9d221a8f60
Aron <aron99ekman@gmail.com>
2023-12-04 23:13:22 +0200

    Day 1

--- day1/main.py
+++ day1/main.py
@@ -1,18 +1,23 @@
-inputFile = open("input.txt", "r")
+inputFile = open("day1/input.txt", "r")
 lines = inputFile.readlines()
 words = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-res = 0
+res_1 = 0
+res_2 = 0
 for line in lines:
-    numbers = []
+    part_1_numbers = []
+    part_2_numbers = []
     for [index, char] in enumerate(line):
         if char.isnumeric():
-            numbers.append(int(char))
+            part_1_numbers.append(int(char))
+            part_2_numbers.append(int(char))
         else:
             for [number, word] in enumerate(words):
                 splitTo = index + len(word)
                 if line[index:splitTo] == word:
-                    numbers.append(number + 1)
+                    part_2_numbers.append(number + 1)
 
-    res += int(f"{numbers[0]}{numbers[-1]}")
+    res_1 += int(f"{part_1_numbers[0]}{part_1_numbers[-1]}")
+    res_2 += int(f"{part_2_numbers[0]}{part_2_numbers[-1]}")
 
-print("part 2:", res)
+print("part 1:", res_1)
+print("part 2:", res_2)
